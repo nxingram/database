922744343c8e711f1776922a443f3ef958eb9cdc
nxingram <nxingram@users.noreply.github.com>
2025-03-19 17:53:52 +0100

    Update db_esempio_mondo.sql

--- dati/db_esempio_mondo.sql
+++ dati/db_esempio_mondo.sql
@@ -1,18 +1,5 @@
 
-
-DROP TABLE IF EXISTS `city`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `city` (
-  `ID` int NOT NULL AUTO_INCREMENT,
-  `Name` char(35) NOT NULL DEFAULT '',
-  `CountryCode` char(3) NOT NULL DEFAULT '',
-  `District` char(20) NOT NULL DEFAULT '',
-  `Population` int NOT NULL DEFAULT '0',
-  PRIMARY KEY (`ID`),
-  KEY `CountryCode` (`CountryCode`),
-  CONSTRAINT `city_ibfk_1` FOREIGN KEY (`CountryCode`) REFERENCES `country` (`Code`)
-) ;
+use elementi25_mondo;
 
 DROP TABLE IF EXISTS `country`;
 CREATE TABLE `country` (
@@ -33,6 +20,20 @@ CREATE TABLE `country` (
   `Code2` char(2) NOT NULL DEFAULT '',
   PRIMARY KEY (`Code`)
 ) ;
+
+DROP TABLE IF EXISTS `city`;
+CREATE TABLE `city` (
+  `ID` int NOT NULL AUTO_INCREMENT,
+  `Name` char(35) NOT NULL DEFAULT '',
+  `CountryCode` char(3) NOT NULL DEFAULT '',
+  `District` char(20) NOT NULL DEFAULT '',
+  `Population` int NOT NULL DEFAULT '0',
+  PRIMARY KEY (`ID`),
+  KEY `CountryCode` (`CountryCode`),
+  CONSTRAINT `city_ibfk_1` FOREIGN KEY (`CountryCode`) REFERENCES `country` (`Code`)
+) ;
+
+
 
 DROP TABLE IF EXISTS `countrylanguage`;
 CREATE TABLE `countrylanguage` (
