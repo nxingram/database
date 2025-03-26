5152308d31fbdf5047d0e9e9945b689d9926ace5
nxingram <nxingram@users.noreply.github.com>
2025-03-26 17:32:40 +0100

    Update db_corso_schema.sql

--- dati/db_corso_schema.sql
+++ dati/db_corso_schema.sql
@@ -1,3 +1,19 @@
+CREATE TABLE `studenti` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(40) NOT NULL,
+  `cognome` varchar(50) NOT NULL,
+  `genere` enum('m','f','nb') DEFAULT NULL,
+  `indirizzo` varchar(100) DEFAULT NULL,
+  `citta` varchar(30) DEFAULT NULL,
+  `provincia` char(2) DEFAULT 'To',
+  `regione` varchar(30) DEFAULT 'Piemonte',
+  `email` varchar(100) NOT NULL,
+  `data_nascita` date DEFAULT NULL,
+  `ins` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
+)
+
 create table if not exists corsi(
 	id int auto_increment,
     titolo varchar(100) not null,
